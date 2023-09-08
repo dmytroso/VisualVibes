5dc6db26c81b87218ab60db9c32e5c6d4f44683c
DmitrySokoltsov <125208868+dmytroso@users.noreply.github.com>
2023-09-08 19:42:50 +0300

    Repository

---
+++ src/main/java/com/vv/VisualVibes/repository/CommentRepository.java
@@ -0,0 +1,14 @@
+package com.vv.VisualVibes.repository;
+
+import com.vv.VisualVibes.entity.Comment;
+import com.vv.VisualVibes.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+    List<Comment> findCommentByPost(Post post);
+    Comment findByIdAndUserId(Long commentId, Long userId);
+}
