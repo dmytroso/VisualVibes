5ae6452bb8b1432da1e3afbc622aff5ca50198e2
DmitrySokoltsov <125208868+dmytroso@users.noreply.github.com>
2023-09-14 22:05:48 +0300

    @Service @Autowired

--- src/main/java/com/vv/VisualVibes/service/CustomUserDetailsService.java
+++ src/main/java/com/vv/VisualVibes/service/CustomUserDetailsService.java
@@ -2,19 +2,24 @@ package com.vv.VisualVibes.service;
 
 import com.vv.VisualVibes.entity.User;
 import com.vv.VisualVibes.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Service
 public class CustomUserDetailsService implements UserDetailsService {
 
     private final UserRepository userRepository;
 
+    @Autowired
     public CustomUserDetailsService(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
