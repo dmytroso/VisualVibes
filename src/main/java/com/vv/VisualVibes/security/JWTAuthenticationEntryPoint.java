e0daebf97acaeecec5ad84ea472152b755c6eb78
DmitrySokoltsov <125208868+dmytroso@users.noreply.github.com>
2023-10-04 19:14:59 +0300

    JWTAuthenticationEntryPoint

---
+++ src/main/java/com/vv/VisualVibes/security/JWTAuthenticationEntryPoint.java
@@ -0,0 +1,26 @@
+package com.vv.VisualVibes.security;
+
+import com.google.gson.Gson;
+import com.vv.VisualVibes.payload.response.InvalidLoginResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import java.io.IOException;
+
+public class JWTAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    @Override
+    public void commence(HttpServletRequest request,
+                         HttpServletResponse response,
+                         AuthenticationException authException) throws IOException, ServletException {
+
+        InvalidLoginResponse invalidLoginResponse = new InvalidLoginResponse();
+        String jsonLoginResponse = new Gson().toJson(invalidLoginResponse);
+        response.setContentType(SecurityConstants.CONTENT_TYPE);
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+        response.getWriter().println(jsonLoginResponse);
+    }
+}
