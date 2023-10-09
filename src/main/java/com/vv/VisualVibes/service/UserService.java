f6da43b2ef8291c716dd8fbdc8b32d47dfbc25c8
DmitrySokoltsov <125208868+dmytroso@users.noreply.github.com>
2023-10-09 14:54:34 +0300

    Create new user service

---
+++ src/main/java/com/vv/VisualVibes/service/UserService.java
@@ -0,0 +1,45 @@
+package com.vv.VisualVibes.service;
+
+import com.vv.VisualVibes.entity.User;
+import com.vv.VisualVibes.entity.role.ERole;
+import com.vv.VisualVibes.exception.UserExistException;
+import com.vv.VisualVibes.payload.request.SignUpRequest;
+import com.vv.VisualVibes.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class UserService {
+    private final Logger LOG = LoggerFactory.getLogger(UserService.class);
+
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
+
+    @Autowired
+    public UserService(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public User createUser(SignUpRequest userIn) {
+        User user = new User();
+        user.setEmail(userIn.getEmail());
+        user.setName(userIn.getFirstname());
+        user.setLastname(userIn.getLastname());
+        user.setUsername(userIn.getUsername());
+        user.setPassword(passwordEncoder.encode(userIn.getPassword()));
+        user.getRoles().add(ERole.ROLE_USER);
+
+        try {
+            LOG.info("Saving User {}", userIn.getEmail());
+            return userRepository.save(user);
+        } catch (Exception e) {
+            LOG.error("Error during registration. {}", e.getMessage());
+            throw  new UserExistException("The user " + userIn.getUsername() + "already exist. Please check credentials");
+        }
+    }
+}
