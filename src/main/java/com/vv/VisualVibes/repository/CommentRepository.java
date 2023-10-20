e24cee02fe0bcd12009cf0a8e655bb03ad6f8522
DmitrySokoltsov <125208868+dmytroso@users.noreply.github.com>
2023-10-20 16:19:21 +0300

    findCommentByPost -> findAllByPost

--- src/main/java/com/vv/VisualVibes/repository/CommentRepository.java
+++ src/main/java/com/vv/VisualVibes/repository/CommentRepository.java
@@ -9,6 +9,6 @@ import java.util.List;
 
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
-    List<Comment> findCommentByPost(Post post);
+    List<Comment> findAllByPost(Post post);
     Comment findByIdAndUserId(Long commentId, Long userId);
 }
